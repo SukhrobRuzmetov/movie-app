c30679df33836e3e698bc6af470abbb82dcbc419
Sukhrobdew <sukhrob2707@gmail.com>
2023-07-04 09:54:44 +0500

    sd

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   const [moviesPerPage] = useState(5);
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=232382f4`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=232382f4`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
